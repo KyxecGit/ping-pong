9184e3deef4430b75dd66027faef4dd398d015cf
KyxecGit <107474711+KyxecGit@users.noreply.github.com>
2024-05-06 15:24:51 +0600

    Update ping_pong.py

--- Pin Pong/ping_pong.py
+++ Pin Pong/ping_pong.py
@@ -11,10 +11,30 @@ class GameSprite(sprite.Sprite):
     def reset(self):
         window.blit(self.image,(self.rect.x,self.rect.y))
 
+
+class Platform(GameSprite):
+
+    def move_left(self):
+        keys = key.get_pressed()
+        if keys[K_UP]:
+            self.rect.y -= self.speed
+        if keys[K_DOWN]:
+            self.rect.y += self.speed
+
+    def move_right(self):
+        keys = key.get_pressed()
+        if keys[K_w]:
+            self.rect.y -= self.speed
+        if keys[K_s]:
+            self.rect.y += self.speed
+
+
 window = display.set_mode((700,500))
 
 
-ball = GameSprite('tenis_ball.png', 300,200, 10, 50,50)
+ball = GameSprite('tenis_ball.png', 300, 200, 10, 50,50)
+player1 = Platform('racket.png', 0, 200, 10, 50,150)
+player2 = Platform('racket.png', 650, 200, 10, 50,150)
 
 speed_x = 3
 speed_y = 3
@@ -29,6 +49,12 @@ while game:
             game = False
 
     window.fill((200,250,250))
+
+    player1.reset()
+    player2.reset()
+    player1.move_left()
+    player2.move_right()
+
     ball.reset()
     ball.rect.x += speed_x
     ball.rect.y += speed_y
